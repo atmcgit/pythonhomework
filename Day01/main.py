e068414dde5c17fbdf4bd2a92854ee66873d5733
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-03-13 00:02:51 +0300

    first commit

---
+++ Day01/main.py
@@ -0,0 +1,69 @@
+print("Kodlamaio")
+baslik = "Taşıt Kredisi"
+print(baslik)
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#int, integer => tamsayı
+vade = 36
+ekVade = 6
+#Metinsel ifade
+vade2 = "36"
+
+#float, decimal, double
+aylikOdeme = 200.50
+
+#bool, boolean => True veya False
+yukselisteMi = True
+yukselisteMi = False
+
+# matematiksel operatörler
+
+# +
+print(5 + 5)
+print(vade + 12)
+print(vade + ekVade)
+print(36 + 6)
+
+
+# -
+print(5 -5)
+print(vade- 12)
+print(vade- ekVade)
+
+# *
+print(5 * 5)
+print(vade * 12)
+print(vade * ekVade)
+print(10 * 10)
+
+# / 
+print(5 / 5)
+print(vade / 12)
+print(vade / ekVade)
+print(10 / 2)
+
+yeniVade = vade / 2
+fiyat = 100
+indirimliFiyat = fiyat -20
+
+print(yeniVade)
+print(indirimliFiyat)
+
+# % => mod operator
+print(10 % 2)
+print(vade % 12)
+print(vade % ekVade)
+print(10 % 2)
+
+# ** mantıksal operatörler
+print(1 == 1)
+print(1 == 2)
+
+print (1 > 2)
+print (1 < 2)
+print (1 >= 1)
+
+print(1 < 2)
+print(3 > 1)
+print(1 < 1)
+print(1 <= 1)

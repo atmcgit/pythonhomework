cbaed37e474b4cc95e75f0e2877537aaae78bfec
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-03-13 18:35:33 +0300

    day one

--- Day01/main.py
+++ Day01/main.py
@@ -1,18 +1,19 @@
+
 print("Kodlamaio")
 baslik = "Taşıt Kredisi"
 print(baslik)
 baslik = "İhtiyaç Kredisi"
 print(baslik)
-#int, integer => tamsayı
+# int, integer => tamsayı
 vade = 36
 ekVade = 6
-#Metinsel ifade
+# Metinsel ifade
 vade2 = "36"
 
-#float, decimal, double
+# float, decimal, double
 aylikOdeme = 200.50
 
-#bool, boolean => True veya False
+# bool, boolean => True veya False
 yukselisteMi = True
 yukselisteMi = False
 
@@ -26,9 +27,9 @@ print(36 + 6)
 
 
 # -
-print(5 -5)
-print(vade- 12)
-print(vade- ekVade)
+print(5 - 5)
+print(vade - 12)
+print(vade - ekVade)
 
 # *
 print(5 * 5)
@@ -36,7 +37,7 @@ print(vade * 12)
 print(vade * ekVade)
 print(10 * 10)
 
-# / 
+# /
 print(5 / 5)
 print(vade / 12)
 print(vade / ekVade)
@@ -44,7 +45,7 @@ print(10 / 2)
 
 yeniVade = vade / 2
 fiyat = 100
-indirimliFiyat = fiyat -20
+indirimliFiyat = fiyat - 20
 
 print(yeniVade)
 print(indirimliFiyat)
@@ -59,39 +60,58 @@ print(10 % 2)
 print(1 == 1)
 print(1 == 2)
 
-print (1 > 2)
-print (1 < 2)
-print (1 >= 1)
+print(1 > 2)
+print(1 < 2)
+print(1 >= 1)
 
 print(1 < 2)
 print(3 > 1)
 print(1 < 1)
 print(1 <= 1)
-print(1 !=1)
-print(1!=2)
+
+
+print(1 != 1)
+print(1 != 2)
 
 # or and => veya
 
-print(1 > 2 or  5 > 2)
+print(1 > 2 or 5 > 2)
 print(1 > 2 and 5 > 2)
-print( 1 > 2 or 5 > 2 and 3 > 2)
+print(1 > 2 or 5 > 2 and 3 > 2)
+
+print(1 > 2 and 5 > 2 and 3 > 2)
+
+print(2 > 1 or 1 > 2 and 3 > 2)
 
 # karar yapıları
-#if else
-sayi1 = 10
+# if else
+
+
+sayi1 = 15
 sayi2 = 15
-# sayi1 sayi2' den büyükse ekrana sayi 1 daha büyük yazdır
-#condition(şart)
+# sayi1 sayi2'den büyükse ekrana sayi1 daha büyüktür yazdır
+# condition şart
 
-#indent
-if sayi1 < sayi2:
-    print("Sayi 1 sayi 2' den küçüktür.")
-
-#eğer if bloğuna girmez ise
+# indent
+if sayi1 <= sayi2:
+    print("Sayı 1 Sayı 2'den büyüktür.")
 elif sayi1 == sayi2:
-    print("İki sayı eşittir")
-    #eğer if ve else if bloklarında hiç birine girmez ise
+    print("İki sayı eşittir.")
+# eğer if ve else if bloklarından hiç birine girmez ise
 else:
-    print(" Sayi 1 Sayi 2' den büyüktür")
+    print("Sayı 1 Sayı 2'den küçüktür.")
+# eğer herhangi bir bloğa girdiyse diğerlerine girmez
 
-print("Burası if bloğuna girmez")
+
+if sayi1<= sayi2:
+    print("Sayi 1 sayi 2'den küçüktür")
+
+#yeni bir yapıya geçtik alttaki ve üstteki if birbirinden bağımsız çalışır.
+
+if sayi1==sayi2:
+    print("İki sayi eşittir")
+else:
+    print("Sayi 1 Sayi 2'den büyüktür.")
+
+
+print("Burası if bloğunun dışıdır.")

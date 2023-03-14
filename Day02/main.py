d11467ff0840a149bb3399a8d418b7ab1b0ad213
Mustafa Atmaca <m.atmaca.official@hotmail.com>
2023-03-15 01:18:21 +0300

    day 2

---
+++ Day02/main.py
@@ -0,0 +1,15 @@
+faiz = 1.59
+vade = "36"
+krediAdı = "İhtiyaç Kredisi"
+
+print(type(faiz))
+print(type(vade))
+print(type(krediAdı))
+
+
+print(int(vade)+12)
+#print(int(krediAdi)) hatalı 
+faiz = str(faiz)
+print(type(faiz))
+
+vade = input("Lütfen isdeğiniz vade sayısını giriniz : ")
